7304485e66937f38d7215d90bfc7a3b07565df5f
Tunde Yakub <tundenasri@gmail.com>
2019-07-12 10:06:48 +0100

    configure project folder

---
+++ src/components/Button/button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button as ReactStrapBtn } from 'reactstrap';
+import './button.scss';
+
+const Button = ({ type, text, onClick, classname }) => (
+  <ReactStrapBtn className={classname} type={type} onClick={onClick}>
+    {text}
+  </ReactStrapBtn>
+);
+
+Button.propTypes = {
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+  text: PropTypes.string.isRequired,
+  classname: PropTypes.string,
+};
+
+Button.defaultProps = {
+  type: 'submit',
+  classname: 'b-button',
+};
+
+export default Button;
